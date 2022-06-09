6c2e782a9e51599d29884b21b4264d74e8a522d4
danielkhtse <daniel+github@sanitysoln.com>
2022-06-09 11:36:18 +0800

    logout page

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -1,18 +1,87 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import type { NextPage } from "next";
+import NextLink from "next/link";
 import Head from "next/head";
-import { Box, Typography } from "@mui/material";
-
+import {
+	Box,
+	Button,
+	Container,
+	Typography,
+	useMediaQuery,
+} from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 import { gtm } from "../lib/gtm";
-import { MainLayout } from "../components/main-layout";
 
 const Logout: NextPage = () => {
+	const theme = useTheme();
+	const mobileDevice = useMediaQuery(theme.breakpoints.down("sm"));
+
+	useEffect(() => {
+		gtm.push({ event: "page_view" });
+	}, []);
+
 	return (
-		<MainLayout>
-			<Typography variant="h2" sx={{ m: 10 }}>
-				Logout
-			</Typography>
-		</MainLayout>
+		<>
+			<Head>
+				<title>
+					Error: Not Found |{" "}
+					{process.env.NEXT_PUBLIC_PAGE_TITLE_SUFFEX}
+				</title>
+			</Head>
+			<Box
+				component="main"
+				sx={{
+					alignItems: "center",
+					backgroundColor: "background.paper",
+					display: "flex",
+					flexGrow: 1,
+					py: "80px",
+				}}>
+				<Container maxWidth="lg">
+					<Typography
+						align="center"
+						variant={mobileDevice ? "h4" : "h1"}>
+						You've been logout
+					</Typography>
+					{/* <Typography
+						align="center"
+						color="textSecondary"
+						sx={{ mt: 0.5 }}
+						variant="subtitle2">
+						You've been logout
+					</Typography> */}
+					{/* <Box
+						sx={{
+							display: "flex",
+							justifyContent: "center",
+							mt: 6,
+						}}>
+						<Box
+							alt="Under development"
+							component="img"
+							src={`/static/error/error404_${theme.palette.mode}.svg`}
+							sx={{
+								height: "auto",
+								maxWidth: "100%",
+								width: 400,
+							}}
+						/>
+					</Box> */}
+					<Box
+						sx={{
+							display: "flex",
+							justifyContent: "center",
+							mt: 6,
+						}}>
+						<NextLink href="/dashboard" passHref>
+							<Button component="a" variant="outlined">
+								Back to Dashboard
+							</Button>
+						</NextLink>
+					</Box>
+				</Container>
+			</Box>
+		</>
 	);
 };
 
