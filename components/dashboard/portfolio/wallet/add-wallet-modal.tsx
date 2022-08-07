ad5126b9c21509e91be6f9431499b0adfea719e2
Yumi Wong <toinyumi@gmail.com>
2022-08-08 00:45:52 +0800

    done

--- components/dashboard/portfolio/wallet/add-wallet-modal.tsx
+++ components/dashboard/portfolio/wallet/add-wallet-modal.tsx
@@ -218,7 +218,7 @@ export const AddWalletDialog: FC = (
             type="submit"
             variant="contained"
           >
-            Finish
+            Add
           </LoadingButton>
         </DialogActions>
       </BootstrapDialog>
