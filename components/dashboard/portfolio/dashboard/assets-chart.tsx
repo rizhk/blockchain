d21333140044a15a86dfcf15487529fb3198a4dc
Yumi Wong <toinyumi@gmail.com>
2022-08-11 03:12:50 +0800

    done

--- components/dashboard/portfolio/dashboard/assets-chart.tsx
+++ components/dashboard/portfolio/dashboard/assets-chart.tsx
@@ -2,10 +2,11 @@ import { useTheme } from '@mui/material';
 import { ApexOptions } from 'apexcharts';
 import { Chart } from 'components/chart';
 import * as React from 'react';
+import { primitivesUtils } from 'utils/primitives-utils';
 
 export interface IAssetsChartProps {
   data: {
-    series: { color: string; data: number; name: string }[];
+    series: { color: string; data: number; name: string; symbol: string }[];
   };
 }
 
@@ -32,7 +33,11 @@ export const AssetsChart: React.FC<IAssetsChartProps> = ({ data }) => {
       },
     },
     tooltip: {
-      enabled: false,
+      enabled: true,
+      custom: function ({ series, seriesIndex, dataPointIndex, w }) {
+        var item = data?.series?.[seriesIndex];
+        return `<div style="padding: 4px">${item.symbol} ${primitivesUtils.convertCurrencyDisplay(item.data)}</div>`;
+      },
     },
     colors: data?.series?.map((item) => item.color) || [],
     labels: data?.series?.map((item) => item.name) || [],
