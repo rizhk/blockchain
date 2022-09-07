beceab797484e6299bde6b29c9b721808ca0e523
danielkhtse <daniel+github@sanitysoln.com>
2022-09-07 14:41:05 +0800

    perform round

--- utils/primitives-utils.ts
+++ utils/primitives-utils.ts
@@ -38,6 +38,33 @@ class PrimitivesUtils {
         .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
     }
   }
+  percentageRounding(number: number | string | undefined, shouldRoundUp: boolean = false) {
+    if (!number) return 0;
+    var val: number = 0;
+    if (typeof number === 'string') val = parseFloat(number);
+    else val = number;
+
+    var dpVal: number;
+    if (number < 1) {
+      dpVal = 4;
+    } else {
+      dpVal = 2;
+      if (shouldRoundUp) {
+        val = Math.round(((val + Number.EPSILON) * Math.pow(10, dpVal)) / Math.pow(10, dpVal));
+      }
+    }
+
+    if (number == 0) {
+      return val.toFixed(2);
+    } else if (number < 1) {
+      return val.toFixed(dpVal);
+    } else {
+      return val
+        .toFixed(dpVal)
+        .toString()
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    }
+  }
   thousandSeparator(number: number | undefined) {
     if (number == undefined) return number;
     return number
@@ -46,7 +73,7 @@ class PrimitivesUtils {
       .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   }
   convertPercentageDisplay(value: string | number, shouldRoundUp: boolean = false): string {
-    return this.numberRounding(value, shouldRoundUp) as string;
+    return this.percentageRounding(value, shouldRoundUp) as string;
   }
   convertCryptoAmountDisplay(value: string | number, tokenSymbol: string, shouldRoundUp: boolean = false): string {
     return (this.numberRounding(value, shouldRoundUp) + ' ' + tokenSymbol) as string;
